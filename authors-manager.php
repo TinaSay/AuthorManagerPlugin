5300258514bafb9b1cf38eb04b18a83a94c49072
TinaSay <tina.sayapina@gmail.com>
2021-04-09 14:25:47 +0500

    Init

---
+++ authors-manager.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Plugin Name:       Authors Manager
+ * Description:       Handles adding new authors, linking them with existing users and display on front end.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Valentina Sayapina
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       authors-manager
+ */
+/**
+ * Authors Manager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * Authors Manager is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Authors Manager. If not, see {URI to Plugin License}.
+ */
+
+
+if (!class_exists('AuthorsManager')) {
+    class AuthorsManager
+    {
+        public function __construct()
+        {
+
+        }
+
+        public function activate()
+        {
+
+        }
+
+        public function deactivate()
+        {
+
+        }
+
+    }
+}
---
+++ uninstall.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mike
+ * Date: 09.04.2021
+ * Time: 14:14
+ */
