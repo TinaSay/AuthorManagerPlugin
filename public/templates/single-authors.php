009931c9d1a9c7141ab66f1f7315ec4e98f5db08
TinaSay <tina.sayapina@gmail.com>
2021-04-12 20:49:21 +0500

    Finished single author page

--- public/templates/single-authors.php
+++ public/templates/single-authors.php
@@ -8,13 +8,7 @@
 require_once plugin_dir_path( __FILE__ ) . '../../inc/class.meta-values.php';
 $authorInfo = new Meta_Values( get_the_ID() );
 
-
-/*echo $authorInfo->firstname ? '<p>' . $authorInfo->firstname . '</p>' : '';
-echo $authorInfo->lastname ? '<p>' . $authorInfo->lastname . '</p>' : '';
-echo $authorInfo->bio ? '<p>' . $authorInfo->bio . '</p>' : '';
-echo $authorInfo->fbUrl ? '<p>' . $authorInfo->fbUrl . '</p>' : '';
-echo $authorInfo->linkedInUrl ? '<p>' . $authorInfo->linkedInUrl . '</p>' : '';*/
-
+$linkedUserNick = get_userdata( $authorInfo->linkedUser )->user_nicename;
 
 get_header(); ?>
 
@@ -28,7 +22,27 @@ get_header(); ?>
 				?>
 
                 <figure class="author-image">
-					<?php echo get_the_post_thumbnail( get_the_ID(), 'large' ); // Featured image ?>
+					<?php
+					if ( has_post_thumbnail() ) {
+						echo get_the_post_thumbnail( get_the_ID(), 'large' ); // Featured image
+					} else { ?>
+                        <img src="<?php echo plugin_dir_url( __FILE__ ) . '../img/avatar.jpg' ?>" alt="">
+					<?php } ?>
+
+                    <div class="soc-links">
+						<?php if ( $authorInfo->fbUrl ): ?>
+                            <a href="<?php echo $authorInfo->fbUrl ?>" target="_blank">
+                                <img src="<?php echo plugin_dir_url( __FILE__ ) . '../img/facebook.png' ?>"
+                                     alt="fb-link">
+                            </a>
+						<?php endif; ?>
+						<?php if ( $authorInfo->linkedInUrl ): ?>
+                            <a href="<?php echo $authorInfo->linkedInUrl ?>" target="_blank">
+                                <img src="<?php echo plugin_dir_url( __FILE__ ) . '../img/linkedin.png' ?>"
+                                     alt="linkedIn-link">
+                            </a>
+						<?php endif; ?>
+                    </div>
                 </figure>
                 <section class="author-info">
                     <h3><?php echo $authorInfo->firstname . ' ' . $authorInfo->lastname ?></h3>
@@ -40,37 +54,43 @@ get_header(); ?>
                 </section>
                 <div class="clearfix"></div>
 
-                <h3>Linked User</h3>
-                <h4>This author is linked to <?php echo get_userdata( $authorInfo->linkedUser )->user_nicename ?></h4>
+                <h1>Related User: <?php echo $linkedUserNick ?></h1>
+                <section class="linked-user">
 
-				<?php
-				// Posts from the WP User the author is linked to
+					<?php
+					// Posts from the WP User the author is linked to
 
-				if ( $authorInfo->linkedUser ) {
-					$query = new WP_Query( [
-						'post_type'      => 'post',
-						'posts_per_page' => 10,
-						'author'         => $authorInfo->linkedUser
-					] );
+					if ( $authorInfo->linkedUser ) {
+						$query = new WP_Query( [
+							'post_type'      => 'post',
+							'posts_per_page' => 10,
+							'author'         => $authorInfo->linkedUser
+						] );
 
-					if ( $query->have_posts() ) {
-						echo 'Posts by ' . get_userdata( $authorInfo->linkedUser )->user_nicename;
-						while ( $query->have_posts() ) {
-							$query->the_post();
+						if ( $query->have_posts() ) {
+							while ( $query->have_posts() ) {
+								$query->the_post();
 
-							echo '<h4>' . get_the_title() . '</h4>';
-							if(get_the_content()!=null){
-								echo '<p>' . wp_trim_words( get_the_content(), 10 ) . '</p>';
+								echo '<div class="post-item">';?>
+                                <h4><a href="<?php echo get_the_permalink() ?>"><?php echo get_the_title() ?></a></h4>
+                                <?php
+
+								echo get_the_post_thumbnail( get_the_ID(), 'thumbnail' );
+								if ( get_the_content() != null ) {
+									echo wp_trim_words( get_the_content(), 55 );
+								}
+								echo '<p class="post-dates">'
+								     . get_the_date() . ' | ' . $linkedUserNick . ' | ' . get_the_category_list( ',' ) . '</p>';
+								echo '</div>';
+
 							}
-							echo '<small>'.get_the_date().'</small>';
-
+						} else {
+							echo 'This user does not have any posts yet';
 						}
-					} else {
-						echo 'This user does not have any posts yet';
 					}
-				}
 
-				?>
+					?>
+                </section>
 			<?php }
 		}
 		?>
