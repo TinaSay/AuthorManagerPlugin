4903723eb60f39538130ebbda89935fc21a4165f
TinaSay <tina.sayapina@gmail.com>
2021-04-14 02:13:39 +0500

    Fixed custom columns shown in admin

--- inc/class.admin-ui.php
+++ inc/class.admin-ui.php
@@ -24,18 +24,18 @@ if ( ! class_exists( 'Admin_Ui' ) ) {
 		}
 
 		// Add the custom columns to the authors post type:
-		public static function set_custom_authors_columns( $columns ) {
+		public static function set_custom_ttp_authors_columns( $columns ) {
 			unset( $columns['date'] );
 			$columns['firstname'] = __( 'First Name' );
 			$columns['lastname']  = __( 'Last Name' );
 			$columns['fbUrl']     = __( 'FB address' );
 			$columns['linkedIn']  = __( 'LinkedIn' );
-			$columns['date']=__('Published');
+			$columns['date']      = __( 'Published' );
 
 			return $columns;
 		}
 
-		public static function custom_authors_column( $column, $post_id ) {
+		public static function custom_ttp_authors_column( $column, $post_id ) {
 			require_once 'class.meta-values.php';
 			$authorInfo = new Meta_Values( $post_id );
 			switch ( $column ) {
@@ -60,8 +60,8 @@ if ( ! class_exists( 'Admin_Ui' ) ) {
 
 	add_action( 'admin_init', [ 'Admin_Ui', 'hideTitle' ] );
 	add_filter( 'gettext', [ 'Admin_Ui', 'renameMediaButton' ], 10, 2 );
-	add_filter( 'manage_authors_posts_columns', [ 'Admin_Ui', 'set_custom_authors_columns' ], 9 );
-	add_action( 'manage_authors_posts_custom_column', [ 'Admin_Ui', 'custom_authors_column' ], 10, 2 );
+	add_filter( 'manage_ttp_authors_posts_columns', [ 'Admin_Ui', 'set_custom_ttp_authors_columns' ], 9 );
+	add_action( 'manage_ttp_authors_posts_custom_column', [ 'Admin_Ui', 'custom_ttp_authors_column' ], 10, 2 );
 
 
 }
