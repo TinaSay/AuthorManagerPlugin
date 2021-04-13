258009aad84e2323f88d9359c307fb205249e46c
TinaSay <tina.sayapina@gmail.com>
2021-04-14 00:53:46 +0500

    Added custom columns to posts table in admin

--- inc/class.admin-ui.php
+++ inc/class.admin-ui.php
@@ -13,17 +13,6 @@ if ( ! class_exists( 'Admin_Ui' ) ) {
 		}
 
 		/**
-		 * Filter posts to make them display author names as titles
-		 */
-		public static function postTitleAsAuthorName( $title, $post_id ) {
-			if ( is_admin() && get_post_type() == 'authors' ) {
-				$title = get_post_meta( $post_id, '_author_name_meta_key', true );
-			}
-
-			return $title;
-		}
-
-		/**
 		 * Rename Add Media button to make it more clear
 		 */
 		public static function renameMediaButton( $translation, $text ) {
@@ -33,9 +22,46 @@ if ( ! class_exists( 'Admin_Ui' ) ) {
 
 			return $translation;
 		}
+
+		// Add the custom columns to the authors post type:
+		public static function set_custom_authors_columns( $columns ) {
+			unset( $columns['date'] );
+			$columns['firstname'] = __( 'First Name' );
+			$columns['lastname']  = __( 'Last Name' );
+			$columns['fbUrl']     = __( 'FB address' );
+			$columns['linkedIn']  = __( 'LinkedIn' );
+			$columns['date']=__('Published');
+
+			return $columns;
+		}
+
+		public static function custom_authors_column( $column, $post_id ) {
+			require_once 'class.meta-values.php';
+			$authorInfo = new Meta_Values( $post_id );
+			switch ( $column ) {
+
+				case 'firstname' :
+					esc_html_e( $authorInfo->firstname );
+					break;
+				case 'lastname' :
+					esc_html_e( $authorInfo->lastname );
+					break;
+				case 'fbUrl':
+					echo esc_url_raw( $authorInfo->fbUrl );
+					break;
+				case 'linkedIn':
+					echo esc_url_raw( $authorInfo->linkedInUrl );
+					break;
+				case 'date':
+					echo get_the_date();
+			}
+		}
 	}
 
 	add_action( 'admin_init', [ 'Admin_Ui', 'hideTitle' ] );
-	add_filter( 'the_title', [ 'Admin_Ui', 'postTitleAsAuthorName' ], 10, 2 );
 	add_filter( 'gettext', [ 'Admin_Ui', 'renameMediaButton' ], 10, 2 );
+	add_filter( 'manage_authors_posts_columns', [ 'Admin_Ui', 'set_custom_authors_columns' ], 9 );
+	add_action( 'manage_authors_posts_custom_column', [ 'Admin_Ui', 'custom_authors_column' ], 10, 2 );
+
+
 }
